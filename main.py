bd47fcaef5934a403c657ea6b85b52b8f94c9fd1
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2020-11-24 06:09:25 +0000

    try to access the voice property

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ class Dog:
   def __init__(self, gender, color):
     self.gender = gender
     self.color = color
+    # Instance method
+    def description(self):
+        return f"{self.gender} is {self.color} "
+
+    # Another instance method
+    def speak(self, sound):
+        return f"{self.gender} says {sound}"
 
 my_dog = Dog('male', 'black')
-print(my_dog.gender)
+my_dog_voice = speak('noise')
+print(my_dog.sound)
+
 
 
 
